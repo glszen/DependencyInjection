a200b356414e61e0307e8dc1c876bed340c9a09a
Gülşen <gulsenk48@hotmail.com>
2024-12-22 17:09:38 +0300

    Add project files.

---
+++ Models/ClassRoom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+    public class ClassRoom
+    {
+        public string Teacher { get; set; } = "";
+       
+        private readonly ITeacher _teacher;
+
+        public void GetTeacherInfo()
+        {
+            _teacher.GetInfo();
+        }
+
+        public ClassRoom(ITeacher teacher)
+        {
+            _teacher = teacher;
+        }
+
+        public void ClassInfo()
+        {
+            Console.WriteLine("Teacher info: ");
+            _teacher.GetInfo();
+        }
+    }
+}
---
+++ Models/ITeacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+    public interface ITeacher
+    {
+         string FirstName { get; set; } 
+         string LastName { get; set; }
+         string GetInfo();
+    }
+}
